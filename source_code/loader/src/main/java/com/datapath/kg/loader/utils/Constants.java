eaabfb852fe113bd45eafef0f3bff2e9dde72b41
andrey_pylypchuk <andrey.pylypchuk@introlab-systems.com>
2020-09-15 16:11:46 +0300

    added source code

---
+++ source_code/loader/src/main/java/com/datapath/kg/loader/utils/Constants.java
@@ -0,0 +1,32 @@
+package com.datapath.kg.loader.utils;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String ACTIVE = "active";
+    public static final String CANCELLED = "cancelled";
+    public static final String UNSUCCESSFUL = "unsuccessful";
+
+    private static final String BIDS_OPENED = "bidsOpened";
+    private static final String EVALUATION_RESULTS_PENDING = "evaluationResultsPending";
+    private static final String EVALUATION_COMPLETE = "evaluationComplete";
+    private static final String CONTRACT_SIGNED = "contractSigned";
+
+    public static final List<String> SKIPPED_BIDS_TENDER_STATUS_DETAILS = Arrays.asList(
+            BIDS_OPENED,
+            EVALUATION_RESULTS_PENDING,
+            EVALUATION_COMPLETE,
+            CONTRACT_SIGNED
+    );
+
+    public static final List<String> SKIPPED_AWARDS_TENDER_STATUS_DETAILS = Arrays.asList(
+            EVALUATION_RESULTS_PENDING,
+            EVALUATION_COMPLETE,
+            CONTRACT_SIGNED
+    );
+}
